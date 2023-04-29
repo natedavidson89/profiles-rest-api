da709837cbd61067af5458f07fe11b207cc37a16
nathan <nathan@madit.io>
2023-04-28 17:08:23 -0700

    Added viewset to profiles api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serializers
 
@@ -45,3 +46,47 @@ class HelloAPiView(APIView):
         """Delete an object"""
         return Response({'method' : "DELETE"})
     
+class HelloViewSet(viewsets.ViewSet):
+    """Test API View Sets"""
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """Return a Hello Message"""
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code'
+        ]
+
+        return Response({'message': "Hello from ViewSet Land!", 'a_viewset' : a_viewset})
+    
+    def create(self, request):
+        """Create a new Hello Message"""
+        serializer =  self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello Mr. {name}, sir!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by it's ID"""
+        return Response({'HTTP_method': 'GET'})
+    
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'http_method': 'PUT'})
+    
+    def partial_update(self, request, pk=None):
+        """Handle updating part of an object"""
+        return Response({'HTTP_method': 'PATCH'})
+    
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+        return Response({"HTTP_method": 'DELETE'})
+        
